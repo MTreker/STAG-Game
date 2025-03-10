e0a6f1942134700f01722b0781c9d95f131d730a
MTreker <150871931+MTreker@users.noreply.github.com>
2025-03-10 12:08:24 -0500

    Delete engine directory

--- engine/Engine.js
+++
@@ -1,3 +0,0 @@
-class Engine{
-  static currentScene
-}
--- engine/Time.js
+++
@@ -1,6 +0,0 @@
-class Time{
-    static msBetweenFrames = 50
-    static fps = 1000 / Time.msBetweenFrames
-    static deltaTime = Time.msBetweenFrames / 1000
-    
-}
