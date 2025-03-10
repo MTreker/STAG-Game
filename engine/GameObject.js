bd242489640924bac2f83cf3086004c0375e54f2
MTreker <150871931+MTreker@users.noreply.github.com>
2025-03-10 12:07:46 -0500

    Add files via upload

--- engine/GameObject.js
+++ engine/GameObject.js
@@ -1,42 +1,14 @@
 class GameObject{
-  components
   constructor(name){
     this.name = name
-    this.components = []
-    this.addComponent(new Transform())
-    this.markForDelete = false
-    this.started = false
-  }
-  get transform(){
-    return this.components[0]
-  }
-  addComponent(component){
-    this.components.push(component)
-    component.parent = this
-    return this
   }
   start(){
-    for(let component of this.components){
-      component.start()
-    }
+
   }
   update(){
-    for(let component of this.components){
-      if(!component.started){
-        component.started = true;
-        component.start()
-      }
-    }
-    for(let component of this.components){
-      component.update()
-    }
+
   }
   draw(){
-    for(let component of this.components){
-      component.draw()
-    }
-  }
-  destroy(){
-    this.markForDelete = true
+
   }
 }
--- engine/Time.js
+++ engine/Time.js
@@ -1,5 +1,5 @@
 class Time{
-    static msBetweenFrames = 20
+    static msBetweenFrames = 50
     static fps = 1000 / Time.msBetweenFrames
     static deltaTime = Time.msBetweenFrames / 1000
     
