dcef462bc3e8af8301a37ee08b8e7f9fb2386c6f
MTreker <150871931+MTreker@users.noreply.github.com>
2025-05-02 21:35:11 -0500

    Add files via upload

--- game/components/MainSceneController.js
+++ game/components/MainSceneController.js
@@ -7,37 +7,37 @@ class MainSceneController extends Component {
         let playerGameObject = Engine.currentScene.findGameObject("Player Game Object")
         let guardGameObjects = Engine.currentScene.findGameObjects("Guard Game Object")
 
-        // for (let guardGameObject of guardGameObjects) {
-        //     // Find BulletMovement component manually
-        //     let bulletComponent1 = guardGameObject.components.find(c => c instanceof GuardMovement1)
-        //     let bulletComponent2 = guardGameObject.components.find(c => c instanceof GuardMovement2)
+        for (let guardGameObject of guardGameObjects) {
+            // Find BulletMovement component manually
+            let bulletComponent1 = guardGameObject.components.find(c => c instanceof GuardMovement1)
+            let bulletComponent2 = guardGameObject.components.find(c => c instanceof GuardMovement2)
 
-        //     if (bulletComponent1) {
-        //         // Horizontal bullet collision
-        //         for (let bullet of bulletComponent1.bullet_count_X) {
-        //             if (Collisions.inCollisionCircle(
-        //                 playerGameObject.transform.x, playerGameObject.transform.y, playerGameObject.transform.r,
-        //                 bullet.x, bullet.y, 15
-        //             )) {
-        //                 Engine.currentScene = new DeathScene()
-        //                 Engine.currentScene.start()
-        //                 return
-        //             }
-        //         }
-        //     }
-        //     if (bulletComponent2) {
-        //         for (let bullet of bulletComponent2.bullet_count_Y) {
-        //             if (Collisions.inCollisionCircle(
-        //                 playerGameObject.transform.x, playerGameObject.transform.y, playerGameObject.transform.r,
-        //                 bullet.x, bullet.y, 15
-        //             )) {
-        //                 Engine.currentScene = new DeathScene()
-        //                 Engine.currentScene.start()
-        //                 return
-        //             }
-        //         }
-        //     }
-        // }
+            if (bulletComponent1) {
+                // Horizontal bullet collision
+                for (let bullet of bulletComponent1.bullet_count_X) {
+                    if (Collisions.inCollisionCircle(
+                        playerGameObject.transform.x, playerGameObject.transform.y, playerGameObject.transform.r,
+                        bullet.x, bullet.y, 15
+                    )) {
+                        Engine.currentScene = new DeathScene()
+                        Engine.currentScene.start()
+                        return
+                    }
+                }
+            }
+            if (bulletComponent2) {
+                for (let bullet of bulletComponent2.bullet_count_Y) {
+                    if (Collisions.inCollisionCircle(
+                        playerGameObject.transform.x, playerGameObject.transform.y, playerGameObject.transform.r,
+                        bullet.x, bullet.y, 15
+                    )) {
+                        Engine.currentScene = new DeathScene()
+                        Engine.currentScene.start()
+                        return
+                    }
+                }
+            }
+        }
 
 
         if (Globals.score >= 5000000000) {
@@ -59,7 +59,7 @@ class MainSceneController extends Component {
 
         Camera.main.transform.x = playerGameObject.transform.x
         Camera.main.transform.y = playerGameObject.transform.y
-        Camera.main.transform.w = 300
+        Camera.main.transform.w = 600
         Camera.main.transform.h = 800
     }
 }
