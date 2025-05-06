6109b5800ac4c2f0d6fbed47943abf2ee00764d9
MTreker <150871931+MTreker@users.noreply.github.com>
2025-05-06 15:16:46 -0500

    Delete game directory

--- game/gameObjects/Boundary.js
+++
@@ -1,7 +0,0 @@
-class Boundary extends GameObject {
-    constructor(name){
-        super(name)
-        this.addComponent(new PhysicsStatic())
-        this.addComponent(new Line("black", 5))
-    }
-}
